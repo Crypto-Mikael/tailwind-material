71cafa09b1a1131e0e9a732ccb0b4dcf6ae15f22
miguel.campos <miguel.campos@ti.hubfloripa.com.br>
2024-12-14 20:10:37 -0300

    chore: updating some indentation

--- README.md
+++ README.md
@@ -9,27 +9,41 @@ Tailwind plugin to use all design tokens from [Material 3](https://m3.material.i
 import { material } from 'tailwind-material';
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [material()], // You can pass the initial pallet obj
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [material()] // You can pass the initial pallet obj
 };
+```
+
+```html
+<!-- index.html  -->
+<!doctype html>
+<html lang="en" data-theme="light">
+	<!-- Add data-theme dark for dark mode light for light mode  -->
+	<head>
+		<meta charset="utf-8" />
+		<link rel="icon" href="favicon.png" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+	</head>
+	<body>
+		....
+	</body>
+</html>
 ```
 
 ```ts
 type InitialPalletColorInHex = {
-  primary?: string;
-  secondary?: string;
-  tertiary?: string;
-  neutral?: string;
-  neutralVariant?: string;
-  error?: string;
+	primary?: string;
+	secondary?: string;
+	tertiary?: string;
+	neutral?: string;
+	neutralVariant?: string;
+	error?: string;
 };
 ```
 
 ## How to use:
 
 ![GIF](https://i.ibb.co/tC1WvmD/Anima-o3.gif)
-
-The library also has basic styled elements and utils as dialogs and support to dark and light theme.
--- src/colors/index.ts
+++ src/colors/index.ts
@@ -1,102 +1,111 @@
-import { CorePalette, CorePaletteColors, Scheme, TonalPalette, rgbaFromArgb } from "@material/material-color-utilities"
-import { MaterialCSSVariables, MaterialCssVariablesOnDOM } from "../base";
+import {
+	CorePalette,
+	CorePaletteColors,
+	Scheme,
+	TonalPalette,
+	rgbaFromArgb
+} from '@material/material-color-utilities';
+import { MaterialCSSVariables, MaterialCssVariablesOnDOM } from '../base';
 
-export const designColorsTokens: {[T in MaterialCSSVariables]: keyof MaterialCssVariablesOnDOM} = {
-  primary: '--md-sys-color-primary',
-  'on-primary': '--md-sys-color-on-primary',
-  'primary-container': '--md-sys-color-primary-container',
-  'on-primary-container': '--md-sys-color-on-primary-container',
-  secondary: '--md-sys-color-secondary',
-  'on-secondary': '--md-sys-color-on-secondary',
-  'secondary-container': '--md-sys-color-secondary-container',
-  'on-secondary-container': '--md-sys-color-on-secondary-container',
-  tertiary: '--md-sys-color-tertiary',
-  'on-tertiary': '--md-sys-color-on-tertiary',
-  'tertiary-container': '--md-sys-color-tertiary-container',
-  'on-tertiary-container': '--md-sys-color-on-tertiary-container',
-  error: '--md-sys-color-error',
-  'on-error': '--md-sys-color-on-error',
-  'error-container': '--md-sys-color-error-container',
-  'on-error-container': '--md-sys-color-on-error-container',
-  background:'--md-sys-color-background',
-  'on-background': '--md-sys-color-on-background',
-  surface: '--md-sys-color-surface',
-  'on-surface': '--md-sys-color-on-surface',
-  'surface-variant': '--md-sys-color-surface-variant',
-  'on-surface-variant': '--md-sys-color-on-surface-variant',
-  outline: '--md-sys-color-outline',
-  'outline-variant': '--md-sys-color-outline-variant',
-  shadow:'--md-sys-color-shadow',
-  scrim: '--md-sys-color-scrim',
-  'inverse-surface': '--md-sys-color-inverse-surface',
-  'inverse-on-surface': '--md-sys-color-inverse-on-surface',
-  'inverse-primary': '--md-sys-color-inverse-primary',
-  'surface-container-lowest': '--md-sys-color-surface-container-lowest',
-  'surface-container-low': '--md-sys-color-surface-container-low',
-  'surface-container': '--md-sys-color-surface-container',
-  'surface-container-high': '--md-sys-color-surface-container-high',
-  'surface-container-highest': '--md-sys-color-surface-container-highest',
-}
+export const designColorsTokens: { [T in MaterialCSSVariables]: keyof MaterialCssVariablesOnDOM } =
+	{
+		primary: '--md-sys-color-primary',
+		'on-primary': '--md-sys-color-on-primary',
+		'primary-container': '--md-sys-color-primary-container',
+		'on-primary-container': '--md-sys-color-on-primary-container',
+		secondary: '--md-sys-color-secondary',
+		'on-secondary': '--md-sys-color-on-secondary',
+		'secondary-container': '--md-sys-color-secondary-container',
+		'on-secondary-container': '--md-sys-color-on-secondary-container',
+		tertiary: '--md-sys-color-tertiary',
+		'on-tertiary': '--md-sys-color-on-tertiary',
+		'tertiary-container': '--md-sys-color-tertiary-container',
+		'on-tertiary-container': '--md-sys-color-on-tertiary-container',
+		error: '--md-sys-color-error',
+		'on-error': '--md-sys-color-on-error',
+		'error-container': '--md-sys-color-error-container',
+		'on-error-container': '--md-sys-color-on-error-container',
+		background: '--md-sys-color-background',
+		'on-background': '--md-sys-color-on-background',
+		surface: '--md-sys-color-surface',
+		'on-surface': '--md-sys-color-on-surface',
+		'surface-variant': '--md-sys-color-surface-variant',
+		'on-surface-variant': '--md-sys-color-on-surface-variant',
+		outline: '--md-sys-color-outline',
+		'outline-variant': '--md-sys-color-outline-variant',
+		shadow: '--md-sys-color-shadow',
+		scrim: '--md-sys-color-scrim',
+		'inverse-surface': '--md-sys-color-inverse-surface',
+		'inverse-on-surface': '--md-sys-color-inverse-on-surface',
+		'inverse-primary': '--md-sys-color-inverse-primary',
+		'surface-container-lowest': '--md-sys-color-surface-container-lowest',
+		'surface-container-low': '--md-sys-color-surface-container-low',
+		'surface-container': '--md-sys-color-surface-container',
+		'surface-container-high': '--md-sys-color-surface-container-high',
+		'surface-container-highest': '--md-sys-color-surface-container-highest'
+	};
 
 const patterns = {
-  colorTones: [0, 5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 95, 98, 99, 100]
+	colorTones: [0, 5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 95, 98, 99, 100]
 } as const;
 
 type schemeProps = {
-  [T in keyof ReturnType<Scheme['toJSON']>]: ColorTones & { DEFAULT: string } | { DEFAULT: string }
+	[T in keyof ReturnType<Scheme['toJSON']>]:
+		| (ColorTones & { DEFAULT: string })
+		| { DEFAULT: string };
 };
 
 type ColorTones = {
-  [T in typeof patterns.colorTones[number]]: string
-}
+	[T in (typeof patterns.colorTones)[number]]: string;
+};
 
 const generateColor = (palette: TonalPalette): ColorTones => {
-  const colorTones = patterns.colorTones.reduce((acc, tone) => {
-    const accTone = rgbaFromArgb(palette.tone(tone));
-    acc[tone] = `rgb(${accTone.r}, ${accTone.g}, ${accTone.b})`;
-    return acc;
-  }, {} as ColorTones);
-  return colorTones;
-}
+	const colorTones = patterns.colorTones.reduce((acc, tone) => {
+		const accTone = rgbaFromArgb(palette.tone(tone));
+		acc[tone] = `rgb(${accTone.r}, ${accTone.g}, ${accTone.b})`;
+		return acc;
+	}, {} as ColorTones);
+	return colorTones;
+};
 
-/** 
+/**
  * @param colorPalette ColorPalette
  * @returns
  * Object that has all tones of a material pallet
- * 
+ *
  * @see
- * Check in the **{@link https://m3.material.io/styles/color/advanced/define-new-colors Define custom color roles}** 
+ * Check in the **{@link https://m3.material.io/styles/color/advanced/define-new-colors Define custom color roles}**
  * topic all tones of the material palette.
-*/
+ */
 const palette = (colorPalette: CorePaletteColors): schemeProps => {
-  const materialPalette = CorePalette.fromColors(colorPalette);
-  let objectPalette: any = {};
-  for (const [key, value] of Object.entries(designColorsTokens)) {
-    const DEFAULT = `rgb(var(${value}) / <alpha-value>)`;
-    switch (key) {
-      case 'primary':
-        objectPalette[key] = { ...generateColor(materialPalette.a1), DEFAULT };
-        break;
-      case 'secondary':
-        objectPalette[key] = { ...generateColor(materialPalette.a2), DEFAULT };
-        break;
-      case 'tertiary':
-        objectPalette[key] = { ...generateColor(materialPalette.a3), DEFAULT };
-        break;
-      case 'neutral':
-        objectPalette[key] = { ...generateColor(materialPalette.n1), DEFAULT };
-        break;
-      case 'neutralVariant':
-        objectPalette[key] = { ...generateColor(materialPalette.n2), DEFAULT };
-        break;
-      case 'error':
-        objectPalette[key] = { ...generateColor(materialPalette.error), DEFAULT };
-        break
-      default:
-        objectPalette[key] = { DEFAULT };
-    }
-  }
-  return objectPalette as schemeProps;
-}
+	const materialPalette = CorePalette.fromColors(colorPalette);
+	const objectPalette = {};
+	for (const [key, value] of Object.entries(designColorsTokens)) {
+		const DEFAULT = `rgb(var(${value}) / <alpha-value>)`;
+		switch (key) {
+			case 'primary':
+				objectPalette[key] = { ...generateColor(materialPalette.a1), DEFAULT };
+				break;
+			case 'secondary':
+				objectPalette[key] = { ...generateColor(materialPalette.a2), DEFAULT };
+				break;
+			case 'tertiary':
+				objectPalette[key] = { ...generateColor(materialPalette.a3), DEFAULT };
+				break;
+			case 'neutral':
+				objectPalette[key] = { ...generateColor(materialPalette.n1), DEFAULT };
+				break;
+			case 'neutralVariant':
+				objectPalette[key] = { ...generateColor(materialPalette.n2), DEFAULT };
+				break;
+			case 'error':
+				objectPalette[key] = { ...generateColor(materialPalette.error), DEFAULT };
+				break;
+			default:
+				objectPalette[key] = { DEFAULT };
+		}
+	}
+	return objectPalette as schemeProps;
+};
 
-export default palette
+export default palette;
